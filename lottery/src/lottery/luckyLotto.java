08118bc05c4a6e9b2616a7f1b5c5a05a98030a94
[SW5] 이윤지 <dbswl342@gmail.com>
2024-03-12 20:31:47 +0900

    내 로또 번호 뽑기 기능 추가

--- lottery/src/lottery/luckyLotto.java
+++ lottery/src/lottery/luckyLotto.java
@@ -7,24 +7,32 @@ public class luckyLotto {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("\n★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆");
-        System.out.println("★☆★☆★☆★☆★☆★☆★행운 복권☆★☆★☆★☆★☆★☆★☆★");
-        System.out.println("★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆");
-        System.out.println("1 또는 2를 입력 후 엔터(Enter)를 입력하세요.");
-        System.out.println("1. 이번 주 당첨 번호 | 2. 랜덤 번호 뽑기");
+        while (true) {
+            System.out.println("\n★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆");
+            System.out.println("★☆★☆★☆★☆★☆★☆★행운 복권☆★☆★☆★☆★☆★☆★☆★");
+            System.out.println("★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆");
+            System.out.println("1 또는 2를 입력 후 엔터(Enter)를 입력하세요.");
+            System.out.println("1. 이번 주 당첨 번호 | 2. 랜덤 번호 뽑기");
 
-        int answer = 0;
-        answer = sc.nextInt();
+            int answer = sc.nextInt();
 
-        if (answer == 1) {
-            // 'weekNum' 클래스의 메서드 호출
-            weekNum.ThisWeekLottoNumbers();
-          
-        } else if (answer == 2) {
-            // 랜덤 번호 뽑기 코드 작성
-        	
-        } else {
-            System.out.println("잘못된 입력입니다. 1 또는 2를 입력하세요.");
+            if (answer == 1) {
+                // 'weekNum' 클래스의 메서드 호출
+                weekNum.ThisWeekLottoNumbers();
+            } else if (answer == 2) {
+                // 'myNum' 클래스의 메서드 호출
+                myNum.myLottoNumbers();
+            } else {
+                System.out.println("잘못된 입력입니다. 1 또는 2를 입력하세요.");
+                continue; // 잘못된 입력일 경우 반복문 처음으로 이동
+            }
+
+            System.out.println("1. 메뉴로 돌아가기 | 2. 종료");
+            int continueAnswer = sc.nextInt();
+
+            if (continueAnswer != 1) {
+                break; // 2를 입력하면 반복문 종료
+            }
         }
 
         sc.close(); // Scanner 사용 후 닫기
---
+++ lottery/src/lottery/myNum.java
@@ -0,0 +1,44 @@
+package lottery;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+public class myNum {
+    public static int[] myLottoNumbers() {
+        // 1. 로또 번호 6자리 배열로 저장
+        int myLotto[] = new int[6];
+
+        // 2. 번호는 1~45
+        int min = 1;
+        int max = 45;
+
+        // 3. 숫자는 랜덤
+        Random ran = new Random();
+
+        // 4. 중복 제거 및 랜덤 숫자 뽑기
+        Set<Integer> ranNumbers = new HashSet<>(); 	// HashSet을 사용하여 중복을 허용하지 않는 정수형 집합 생성 
+        while (ranNumbers.size() < myLotto.length) {
+            int randomNumber = ran.nextInt((max - min) + 1) + min; // random 클래스를 사용하여 난수 생성
+            ranNumbers.add(randomNumber); // 생성된 난수를 randomNumber 집합에 추가 
+        }
+
+        // 5 집합 -> 배열로 값 복사
+        Integer[] weekNumbersArray = ranNumbers.toArray(new Integer[0]);
+        for (int i = 0; i < myLotto.length; i++) {
+        	myLotto[i] = weekNumbersArray[i].intValue(); // Integer를 int로 변환하여 복사
+        }
+
+        // 6. 정렬
+        Arrays.sort(myLotto);
+        
+        // 로또 번호 출력
+        System.out.println("이번 주 로또 번호는 ~~~?? ");
+        for (int number : myLotto) {
+            System.out.print(number + " ");
+        }
+
+        return myLotto;
+    }
+}
